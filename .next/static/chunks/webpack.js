b497843596920275a3b31c6e776c3532a22f198e
Mrs. Zwickermann <azimmermann@epages.com>
2021-02-27 11:20:58 +0100

    Update: Adapt Dev reference

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "37241d79290a6efc388b";
+/******/ 	var hotCurrentHash = "2de91c9d6b4cf4d1a433";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- pages/development.js
+++ pages/development.js
@@ -1,5 +1,6 @@
 import About from "../components/About"
 import Head from 'next/head'
+import Image from "next/image";
 import styles from '../styles/Development.module.css'
 
 const Development = () => {
@@ -22,19 +23,17 @@ const Development = () => {
 
                 <div className={styles.content}>
                     <div className={styles.box}>
+                        <a target="_blank" href="https://dashboardz.vercel.app/"><Image width={900} height={600} src="/dev-dashboard.png" alt="Custome dashboard implementation with ToDo list" /></a>
+                    </div>
+                    <div className={styles.boxText}>
                         <b>Coming soon... </b><br/> 
                         The ONE to rule them all API <br/> 
-                        <a className="link" target="_blank" href="https://github.com/azwick/lord-of-the-rings">See on GitHub</a>
+                        <a className="link" target="_blank" href="https://github.com/azwick/lord-of-the-rings">See progress on GitHub</a>
                      </div>
-                    <div className={styles.box}>
-                        <b>Coming soon... </b><br/> 
-                        Dashboard playground <br/> 
-                        <a className="link" target="_blank" href="https://github.com/azwick/playground/tree/master/dashboard">See on GitHub</a>
-                    </div>
-                    <div className={styles.box}>
+                    <div className={styles.boxText}>
                         <b>Coming soon... </b><br/> 
                         Habits App <br/> 
-                        <a className="link" target="_blank" href="https://github.com/azwick/habits-app">See on GitHub</a>
+                        <a className="link" target="_blank" href="https://github.com/azwick/habits-app">See progress on GitHub</a>
                     </div>
                     {/* <div className={styles.box}>Coming soon... <br/> Jokes API Playground</div>
                     <div className={styles.box}>Coming soon... <br/> Memory game</div>
